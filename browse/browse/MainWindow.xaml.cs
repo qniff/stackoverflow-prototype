b243abdb57c2782b13d4c6113d29ea436e2488fe
seung.eun__ <38011960+HeoSeungeun@users.noreply.github.com>
2019-10-18 14:55:35 -0600

    Question browser
    question browse + database

---
+++ browse/browse/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace browse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string connectionString = "server=localhost;" +
+                                    "database=assignment;" +
+                                    "user id=root;" +
+                                    "password=pass;";
+        public MainWindow()
+        {
+            InitializeComponent();
+            getQuestions();
+        }
+
+        public void getQuestions()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            string query = "select * from dbo.question";
+            using(SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    string title = reader.GetString(1);
+                    string description = reader.GetString(2);
+                    question q = new question(title, description);
+                    stkMain.Children.Add(q);
+                }
+            }
+        }
+
+    }
+}
---
+++ browse/browse/question.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace browse
+{
+    /// <summary>
+    /// Interaction logic for question.xaml
+    /// </summary>
+    public partial class question : UserControl
+    {
+        public question(string title, string description)
+        {
+            InitializeComponent();
+            lblTitle.Content = title;
+            lblDescription.Content = description;
+        }
+    }
+}
