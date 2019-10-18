3aa3aec4d7630e7df2419b036169ac838ed6d2f7
seung.eun__ <38011960+HeoSeungeun@users.noreply.github.com>
2019-10-18 13:49:48 -0600

    PrototypeDemo
    scrollbox demo file

---
+++ PrototypeDemo/PrototypeDemo/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrototypeDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTest_Click(object sender, RoutedEventArgs e)
+        {
+            Post p = new Post("Test Title", "Lorem ipsum dolar", DateTime.Now.ToString(), "Test user");
+            stkPosts.Children.Add(p);
+        }
+    }
+}
